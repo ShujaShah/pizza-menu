7c02b2c309c8f6a4c16a25b90097e9549e3da719
shuja-shah3 <shuja.shah@iquasar.com>
2024-03-05 12:19:32 +0530

    implemented lists

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,24 @@
+import pizzaData from '../../public/data';
 import Pizzas from './Pizzas';
 
 const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Pizzas</h2>
-      <Pizzas
+      {pizzaData.map((pizza) => (
+        <Pizzas
+          name={pizza.name}
+          ingredient={pizza.ingredients}
+          photoName={pizza.photoName}
+          price={pizza.price}
+        />
+      ))}
+      {/* <Pizzas
         name="Pizza Spinacci"
         ingredient="Tomato, mozarella, spinach and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
         price={12}
-      />
+      /> */}
     </main>
   );
 };
