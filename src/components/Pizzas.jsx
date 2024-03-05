9e79d80526c22807845739d465e578067c2188bf
shuja-shah3 <shuja.shah@iquasar.com>
2024-03-05 11:47:54 +0530

    passing data other way

--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -1,11 +1,26 @@
-const Pizzas = (props) => {
+// const Pizzas = (props) => {
+//   return (
+//     <div className="pizza">
+//       <img src={props.photoName} alt={props.name} />
+//       <div>
+//         <h3>{props.name}</h3>
+//         <p>{props.ingredient}</p>
+//         <span> ${props.price}</span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Pizzas;
+
+const Pizzas = ({ name, photoName, ingredient, price }) => {
   return (
     <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+      <img src={photoName} alt={name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredient}</p>
-        <span> ${props.price}</span>
+        <h3>{name}</h3>
+        <p>{ingredient}</p>
+        <span> ${price}</span>
       </div>
     </div>
   );
