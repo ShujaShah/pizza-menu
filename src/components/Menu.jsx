a6a36158e79d9c27f4232d5ab5fc927f37208bf8
shuja-shah3 <shuja.shah@iquasar.com>
2024-03-05 12:38:56 +0530

    styling changes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,21 +4,12 @@ import Pizzas from './Pizzas';
 const Menu = () => {
   return (
     <main className="menu">
-      <h2>Our Pizzas</h2>
-      {pizzaData.map((pizza) => (
-        <Pizzas
-          name={pizza.name}
-          ingredient={pizza.ingredients}
-          photoName={pizza.photoName}
-          price={pizza.price}
-        />
-      ))}
-      {/* <Pizzas
-        name="Pizza Spinacci"
-        ingredient="Tomato, mozarella, spinach and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={12}
-      /> */}
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizzas key={pizza.name} pizzaObj={pizza} />
+        ))}
+      </ul>
     </main>
   );
 };
--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -1,28 +1,13 @@
-// const Pizzas = (props) => {
-//   return (
-//     <div className="pizza">
-//       <img src={props.photoName} alt={props.name} />
-//       <div>
-//         <h3>{props.name}</h3>
-//         <p>{props.ingredient}</p>
-//         <span> ${props.price}</span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Pizzas;
-
-const Pizzas = ({ name, photoName, ingredient, price }) => {
+const Pizzas = ({ pizzaObj }) => {
   return (
-    <div className="pizza">
-      <img src={photoName} alt={name} />
+    <li className="pizza">
+      <img src={pizzaObj.photoName} alt={name} />
       <div>
-        <h3>{name}</h3>
-        <p>{ingredient}</p>
-        <span> ${price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span> ${pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 };
 
